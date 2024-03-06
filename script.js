f29c24669bed3fe211e241309f6be4c846781003
Dani F <114934334+Yuf0ria@users.noreply.github.com>
2024-03-06 23:31:23 +0800

    update index and shop;

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let scrollContainer = document.querySelector("#products .pro");
+let scrollContainer = document.querySelector(".pro-container");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
 scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
 });
+
+nextBtn.addEventListener("click", () =>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+})
+
+nextBtn.addEventListener("click", () =>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+})
