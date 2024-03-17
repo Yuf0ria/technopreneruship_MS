b657eb03d446bef665e5bf9afd3c6cc0f4b40201
Dani F <114934334+Yuf0ria@users.noreply.github.com>
2024-03-18 02:58:16 +0800

    Finished Index will now move to shop;

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let scrollContainer = document.querySelector(".pro-container");
-let backBtnBtn = document.getElementById("backBtn");
+//Left and Right Arrows in index.html
+let scrollContainer = document.querySelector(".pro-container"); //variable of container
+let backBtnBtn = document.getElementById("backBtn"); //id of the icon
 let nextBtn = document.getElementById("nextBtn");
 
+//Move using wheel
 scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
     scrollContainer.style.scrollBehavior = "auto";
 });
 
+//move using the arrows, lacking smoothness haven't figured that out
 nextBtn.addEventListener("click", () =>{
     scrollContainer.style.scrollBehavior = "inherit";
     scrollContainer.scrollLeft += 200;
@@ -17,3 +20,20 @@ backBtn.addEventListener("click", () =>{
     scrollContainer.style.scrollBehavior = "inherit";
     scrollContainer.scrollLeft -= 200;
 });
+
+//mobile navigation port, stle.css
+const bar = document.getElementById('bar');//id variable of the bar icon
+const close = document.getElementById('close');//id variable of the close icon in nav
+const nav = document.getElementById('navbar'); //navigation bar in the mobile port
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}
